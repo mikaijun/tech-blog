509cffab32162be6e4bb160f7530abaf32a8e67c
mikaijun <mikai@anyinc.jp>
2022-11-20 08:06:38 +0900

    sqliteの接続

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './modules/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemModule } from './item/item.module';
+import { Item } from './item/item.entity';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -9,6 +12,13 @@ import { UsersModule } from './modules/users.module';
       typePaths: ['./**/*.graphql'],
       playground: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/sqlitedb.db',
+      synchronize: true,
+      entities: [Item],
+    }),
+    ItemModule,
     UsersModule,
   ],
 })
